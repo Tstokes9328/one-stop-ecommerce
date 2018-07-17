8e05c4879564f90fb6264aec8c91318b22c58bdb
Tayte Stokes <taytestokes9328@gmail.com>
2018-07-16 18:40:35 -0600

    Setup HashRouter landing on the LandingPage

---
+++ one-stop/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//Styling
+import './LandingPage.css';
+
+function landingPage(){
+  return (
+    <div className="landingPage-container">
+        
+    </div>
+  )
+}
+
+export default landingPage;
